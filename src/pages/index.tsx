9f19402fdb103faacc64e52dbcb49ac2a54f9e18
azurity <784227594@qq.com>
2019-03-29 13:22:21 +0800

    一些修复

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -172,12 +172,12 @@ class Index extends React.Component<IndexProps, IndexState> {
                 cache: 'no-cache'
             })
         }
-        this.props.history.replace(/*constValue.rootPath + */id)
+        this.props.history.replace(/*constValue.rootPath + */ '/' + id)
     }
     handleSure = () => {
         this.go(this.state.tempId)
         this.setState({
-            create: false,
+            create: this.state.tempId[9] == '-',
             dirty: false,
             openDialog: false,
             tempId: '_'
